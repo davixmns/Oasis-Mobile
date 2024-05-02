937fd892a9885ae2e5f9b1231590079dc3f7cbf6
Davi Ximenes <82062555+davixmns@users.noreply.github.com>
2024-05-01 22:34:17 -0300

    :art: tela de settings

--- screens/Settings.tsx
+++ screens/Settings.tsx
@@ -3,12 +3,15 @@ import {Container, Content} from "./Styles";
 import {useNavigation} from "@react-navigation/native";
 import {useAuthContext} from "../contexts/AuthContext";
 import {Button} from "react-native-paper";
-import {Alert, ScrollView, Text, TouchableOpacity, View} from "react-native";
+import {Alert, Linking, ScrollView, Text, TouchableOpacity, View} from "react-native";
 import styled from "styled-components/native";
 import {FontAwesome6} from "@expo/vector-icons";
 
 export default function Settings() {
     const {user, signOut} = useAuthContext()
+    const goToLinkendin = () => Linking.openURL('https://www.linkedin.com/in/davi-ximenes-93314a20b/')
+    const goToGithub = () => Linking.openURL('https://github.com/davixmns')
+    const goToEmail = () => Linking.openURL('mailto:davixmnsl@gmail.com')
 
     function handleSignOut() {
         Alert.alert('Sign Out', 'Are you sure you want to sign out?', [
@@ -26,76 +29,77 @@ export default function Settings() {
     return (
         <SettingsContainer>
             <ScrollContainer>
-                <OptionsContainer>
-                    <SectionTitle>ACCOUNT</SectionTitle>
-                    <OptionBox>
-                        <OptionBoxContent>
-                            <Icon name={'user'}/>
-                            <OptionsTitle>User Name</OptionsTitle>
-                        </OptionBoxContent>
-                        <OptionUserData>{user?.name}</OptionUserData>
-                    </OptionBox>
-                    <OptionBox>
-                        <OptionBoxContent>
-                            <Icon name={'envelope'}/>
-                            <OptionsTitle>Email</OptionsTitle>
-                        </OptionBoxContent>
-                        <OptionUserData>{user?.email}</OptionUserData>
-                    </OptionBox>
-                    <OptionBox>
-                        <OptionBoxContent>
-                            <Icon name={'square-plus'}/>
-                            <OptionsTitle>Subscription</OptionsTitle>
-                        </OptionBoxContent>
-                        <OptionUserData>Free Plan</OptionUserData>
-                    </OptionBox>
-                </OptionsContainer>
-                <OptionsContainer>
-                    <SectionTitle>APP</SectionTitle>
-                    <OptionBox>
-                        <OptionBoxContent>
-                            <Icon name={'moon'}/>
-                            <OptionsTitle>Color Scheme</OptionsTitle>
-                        </OptionBoxContent>
-                        <OptionUserData>Dark</OptionUserData>
-                    </OptionBox>
-                    <OptionBox>
-                        <OptionBoxContent>
-                            <Icon name={'earth-americas'}/>
-                            <OptionsTitle>Main Language</OptionsTitle>
-                        </OptionBoxContent>
-                        <OptionUserData>English</OptionUserData>
-                    </OptionBox>
-                </OptionsContainer>
-                <OptionsContainer>
-                    <SectionTitle>DEVELOPER</SectionTitle>
-                    <OptionBox>
-                        <OptionBoxContent>
-                            <Icon name={'at'}/>
-                            <OptionsTitle>Davi Ximenes</OptionsTitle>
-                        </OptionBoxContent>
-                        <OptionUserData>Software Enginner</OptionUserData>
-                    </OptionBox>
-                </OptionsContainer>
-                <SquareContainer>
-                    <SquareButton>
-                        <FontAwesome6 name={'linkedin'} size={25} color={'#fff'}/>
-                    </SquareButton>
-                    <SquareButton>
-                        <FontAwesome6 name={'github'} size={25} color={'#fff'}/>
-                    </SquareButton>
-                    <SquareButton>
-                        <FontAwesome6 name={'envelope'} size={25} color={'#fff'}/>
-                    </SquareButton>
-                </SquareContainer>
-                <OptionsContainer>
+                <SettingsContent>
+
+                    <OptionsContainer>
+                        <SectionTitle>ACCOUNT</SectionTitle>
+                        <OptionBox>
+                            <OptionBoxContent>
+                                <Icon name={'user'}/>
+                                <OptionsTitle>User Name</OptionsTitle>
+                            </OptionBoxContent>
+                            <OptionUserData>{user?.name}</OptionUserData>
+                        </OptionBox>
+                        <OptionBox>
+                            <OptionBoxContent>
+                                <Icon name={'envelope'}/>
+                                <OptionsTitle>Email</OptionsTitle>
+                            </OptionBoxContent>
+                            <OptionUserData>{user?.email}</OptionUserData>
+                        </OptionBox>
+                        <OptionBox>
+                            <OptionBoxContent>
+                                <Icon name={'square-plus'}/>
+                                <OptionsTitle>Subscription</OptionsTitle>
+                            </OptionBoxContent>
+                            <OptionUserData>Free Plan</OptionUserData>
+                        </OptionBox>
+                    </OptionsContainer>
+                    <OptionsContainer>
+                        <SectionTitle>APP</SectionTitle>
+                        <OptionBox>
+                            <OptionBoxContent>
+                                <Icon name={'moon'}/>
+                                <OptionsTitle>Color Scheme</OptionsTitle>
+                            </OptionBoxContent>
+                            <OptionUserData>Dark</OptionUserData>
+                        </OptionBox>
+                        <OptionBox>
+                            <OptionBoxContent>
+                                <Icon name={'earth-americas'}/>
+                                <OptionsTitle>Main Language</OptionsTitle>
+                            </OptionBoxContent>
+                            <OptionUserData>English</OptionUserData>
+                        </OptionBox>
+                    </OptionsContainer>
+                    <OptionsContainer>
+                        <SectionTitle>DEVELOPER</SectionTitle>
+                        <OptionBox>
+                            <OptionBoxContent>
+                                <Icon name={'at'}/>
+                                <OptionsTitle>Davi Ximenes</OptionsTitle>
+                            </OptionBoxContent>
+                            <OptionUserData>Software Enginner</OptionUserData>
+                        </OptionBox>
+                    </OptionsContainer>
+                    <SquareContainer>
+                        <SquareButton onPress={goToLinkendin}>
+                            <FontAwesome6 name={'linkedin'} size={25} color={'#fff'}/>
+                        </SquareButton>
+                        <SquareButton onPress={goToGithub}>
+                            <FontAwesome6 name={'github'} size={25} color={'#fff'}/>
+                        </SquareButton>
+                        <SquareButton onPress={goToEmail}>
+                            <FontAwesome6 name={'envelope'} size={25} color={'#fff'}/>
+                        </SquareButton>
+                    </SquareContainer>
                     <SignOutBox onPress={handleSignOut}>
                         <OptionBoxContent>
                             <Icon name={'arrow-right-from-bracket'}/>
                             <OptionsTitle>Sign Out</OptionsTitle>
                         </OptionBoxContent>
                     </SignOutBox>
-                </OptionsContainer>
+                </SettingsContent>
             </ScrollContainer>
         </SettingsContainer>
     )
@@ -137,15 +141,17 @@ const ScrollContainer = styled(ScrollView).attrs({
     contentContainerStyle: {
         backgroundColor: '#1a1b1d',
         flex: 1,
+        width: '100%',
         display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        justifyContent: 'flex-start',
-        marginTop: 20,
-        width: '90%',
-        gap: 25
+        alignItems: 'center',
     }
 })``
+
+const SettingsContent = styled.View`
+  width: 90%;
+  margin-top: 20px;
+  gap: 25px;
+`
 
 const OptionsContainer = styled.View`
   width: 100%;
@@ -164,8 +170,7 @@ const OptionBox = styled.View`
   padding-right: 15px;
 `
 
-const SignOutBox = styled(TouchableOpacity)`
-  width: 100%;
+const SignOutBox = styled.TouchableOpacity`
   display: flex;
   align-items: center;
   flex-direction: row;
